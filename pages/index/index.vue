ce59e404569f728d4ca58052161e79fc58eaf62a
loverchian <101740536+loverchian@users.noreply.github.com>
2022-09-28 13:11:42 -0400

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		hello uni-app
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
